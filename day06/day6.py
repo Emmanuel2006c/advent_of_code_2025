6b2acc482649b15449cd5060cb98398dc34bc298
Emmanuel Maragakis <emaragakis2006@gmail.com>
2025-12-17 14:41:38 +0000

    finished day4

---
+++ day06/day6.py
@@ -0,0 +1,14 @@
+import time,re
+
+def loadfile(file):
+    with open(file) as f:
+        
+
+
+def solve(p)
+    return p
+
+
+time_start = time.perf_counter()
+print(f"Solution. {solve(loadfile(day6.txt))}")
+print(f"Thought for {time_start - time.perf_counter:.6f} Seconds")
